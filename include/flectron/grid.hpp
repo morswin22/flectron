c4c6bad4301806723d2fda7be37dba951723f50a
morswin22 <xmorswinx@gmail.com>
2021-12-04 23:55:22 +0100

    :zap: Improve performance of spatial hash grid

--- include/flectron/grid.hpp
+++ include/flectron/grid.hpp
@@ -9,13 +9,16 @@ namespace flectron
 
   using ULL = unsigned long long;
   using UL = unsigned long;
+  using ClientIndices = std::pair<std::pair<int,int>, std::pair<int,int>>;
 
   class SpatialHashGrid 
   {
   private:
     std::unordered_map<ULL, std::unordered_set<Ref<Body>>> cells;
-    std::unordered_map<Ref<Body>, std::pair<std::pair<int,int>, std::pair<int,int>>> cellIndices;
+    std::unordered_map<Ref<Body>, ClientIndices> clientIndices;
+    std::unordered_map<Ref<Body>, int> clientQuery;
     int cellSize;
+    int queryIdentifier;
 
   public:
     SpatialHashGrid(int cellSize);
@@ -23,7 +26,7 @@ namespace flectron
     void insert(Ref<Body>& body);
     void remove(const Ref<Body>& body);
     
-    std::unordered_set<Ref<Body>> getCells(const AABB& aabb);
+    std::vector<Ref<Body>> getCells(const AABB& aabb);
 
   private:
     inline ULL getHash(int a, int b) { return (ULL)a << 32 | (UL)b; };
--- src/grid.cpp
+++ src/grid.cpp
@@ -4,7 +4,7 @@ namespace flectron
 {
   
   SpatialHashGrid::SpatialHashGrid(int cellSize)
-    : cells(), cellSize(cellSize)
+    : cells(), cellSize(cellSize), queryIdentifier(0)
   {}
 
   void SpatialHashGrid::insert(Ref<Body>& body)
@@ -16,12 +16,13 @@ namespace flectron
     int maxX = (int)floor(aabb.max.x / cellSize);
     int maxY = (int)floor(aabb.max.y / cellSize);
 
-    if (cellIndices.count(body) != 0)
+    if (clientIndices.count(body) != 0)
     {
-      int lastMinX = cellIndices[body].first.first;
-      int lastMinY = cellIndices[body].first.second;
-      int lastMaxX = cellIndices[body].second.first;
-      int lastMaxY = cellIndices[body].second.second;
+      const ClientIndices& indices = clientIndices[body];
+      int lastMinX = indices.first.first;
+      int lastMinY = indices.first.second;
+      int lastMaxX = indices.second.first;
+      int lastMaxY = indices.second.second;
 
       if (lastMinX == minX && lastMinY == minY && lastMaxX == maxX && lastMaxY == maxY)
         return;
@@ -29,7 +30,8 @@ namespace flectron
       remove(body);
     }
 
-    cellIndices[body] = { {minX, minY}, {maxX, maxY} };
+    clientIndices[body] = { {minX, minY}, {maxX, maxY} };
+    clientQuery[body] = -1;
 
     for (int x = minX; x <= maxX; x++)
     {
@@ -40,19 +42,29 @@ namespace flectron
     }
   }
 
-  std::unordered_set<Ref<Body>> SpatialHashGrid::getCells(const AABB& aabb)
+  std::vector<Ref<Body>> SpatialHashGrid::getCells(const AABB& aabb)
   {
     int minX = (int)floor(aabb.min.x / cellSize);
     int minY = (int)floor(aabb.min.y / cellSize);
     int maxX = (int)floor(aabb.max.x / cellSize);
     int maxY = (int)floor(aabb.max.y / cellSize);
 
-    std::unordered_set<Ref<Body>> result;
+    std::vector<Ref<Body>> result;
+    const int id = queryIdentifier++;
+
     for (int x = minX; x <= maxX; x++)
     {
       for (int y = minY; y <= maxY; y++)
       {
-        result.insert(cells[getHash(x, y)].begin(), cells[getHash(x, y)].end());
+        for (const Ref<Body>& body : cells[getHash(x, y)])
+        {
+          int& clientQueryId = clientQuery[body];
+          if (clientQueryId != id)
+          {
+            clientQueryId = id;
+            result.push_back(body);
+          }
+        }
       }
     }
 
@@ -61,10 +73,11 @@ namespace flectron
 
   void SpatialHashGrid::remove(const Ref<Body>& body)
   {
-    int minX = cellIndices[body].first.first;
-    int minY = cellIndices[body].first.second;
-    int maxX = cellIndices[body].second.first;
-    int maxY = cellIndices[body].second.second;
+    const ClientIndices& indices = clientIndices[body];
+    int minX = indices.first.first;
+    int minY = indices.first.second;
+    int maxX = indices.second.first;
+    int maxY = indices.second.second;
 
     for (int x = minX; x <= maxX; x++)
     {
@@ -74,7 +87,8 @@ namespace flectron
       }
     }
 
-    cellIndices.erase(body);
+    clientIndices.erase(body);
+    clientQuery.erase(body);
   }
 
 }
